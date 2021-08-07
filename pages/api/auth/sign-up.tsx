3c75e7cdf93893c8f6fd8e37e417aa6ccec0423f
alejandro aguilar higuera <aaguilar@adhocti.com>
2021-08-07 00:51:19 -0600

    Se detalla mas los datos de prueba login, sign-up

--- pages/api/auth/sign-up.tsx
+++ pages/api/auth/sign-up.tsx
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse): void {
-  return res.status(200).json({
+  return res.status(201).json({
     token: 'basdfasd',
     refreshToken: '',
     firebaseToken: '',
