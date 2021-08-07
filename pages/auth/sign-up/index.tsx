a7bcf0502b36be68ca9250e9385769e189922d68
alejandro aguilar higuera <aaguilar@adhocti.com>
2021-08-07 00:53:37 -0600

    Se agrega el formulario para registro nuevo de usuarios

--- pages/auth/sign-up/index.tsx
+++ pages/auth/sign-up/index.tsx
@@ -1,7 +1,77 @@
-export default function signUp(): JSX.Element {
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthService } from '../../../services/API';
+
+export interface Prop {
+  callback?: string;
+}
+
+const authService = new AuthService();
+
+export default function SignUp(): JSX.Element {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>('');
+  const [displayName, setDisplayName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const {
+    callback,
+  }: Prop = router.query;
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const session = await authService.signUp(email, password, displayName);
+
+    if (session?.token) {
+      router.push(callback || '/dashboard');
+    }
+  }
+
   return (
-    <h1>
-      sign up
-    </h1>
+    <div className="container">
+
+      <h1>Sign up </h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="displayName">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            autoFocus
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Registrar
+        </Button>
+
+      </Form>
+    </div>
   );
 }
