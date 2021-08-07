0a38d6d74d4f7ee7ae71fc10149ddcff2fe05620
alejandro aguilar higuera <aaguilar@adhocti.com>
2021-08-07 00:51:38 -0600

    Se agrega la dirección del api prueba

--- config/index.tsx
+++ config/index.tsx
@@ -1,4 +1,8 @@
+// Local test
 export const API_URL = 'http://localhost:3000/api';
+
+// Integración con el api local
+// export const API_URL = 'http://localhost:3009';
 
 export default {
   API_URL,
